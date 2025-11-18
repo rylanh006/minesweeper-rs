b84451daa06c9a61bd98282568d7cebf87b028ff
Rylan Hillman <rylanhillman@gmail.com>
2025-11-18 19:08:56 +0100

    Remove nested project directory

--- minesweeper-rs/target/debug/deps/cfg_if-a8b587f74387df5b.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Rylan\minesweeper-rs\minesweeper-rs\target\debug\deps\cfg_if-a8b587f74387df5b.d: C:\Users\rylan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\Rylan\minesweeper-rs\minesweeper-rs\target\debug\deps\libcfg_if-a8b587f74387df5b.rlib: C:\Users\rylan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\Rylan\minesweeper-rs\minesweeper-rs\target\debug\deps\libcfg_if-a8b587f74387df5b.rmeta: C:\Users\rylan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\rylan\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
